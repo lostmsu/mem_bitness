1ce3f1d5e60e7345604d01a2b6520b2fa49e21b7
Victor Milovanov <lostfreeman@gmail.com>
2018-05-01 13:08:01 -0700

    manually implemented PartialEq and PartialOrd for TypedPtr

--- src/typed_ptr.rs
+++ src/typed_ptr.rs
@@ -1,17 +1,26 @@
+use std::cmp::Ordering;
 use std::marker::PhantomData;
 use memory::Memory;
 
-#[derive(PartialOrd, PartialEq, Copy, Clone, Debug)]
-pub struct TypedPtr<T, PTR: Copy + PartialOrd + Clone + PartialEq>{
+#[derive(Copy, Clone, Debug)]
+pub struct TypedPtr<T, PTR>{
     ptr: PTR,
     phantom: PhantomData<T>,
 }
 
-impl<T, PTR: Copy + PartialOrd + Clone + PartialEq> TypedPtr<T, PTR> {
+impl<T, PTR: Copy> TypedPtr<T, PTR> {
     pub unsafe fn new(address: PTR) -> Self { TypedPtr { ptr: address, phantom: PhantomData } }
 
     pub fn address(&self) -> PTR { self.ptr }
 
     pub unsafe fn read<MEM: Memory<PTR>>(&self, mem: &MEM) -> T { mem.read(self.ptr) }
     pub unsafe fn write<MEM: Memory<PTR>>(&self, mem: &mut MEM, value: T) { mem.write(self.ptr, value) }
+}
+
+impl<T, PTR: PartialEq> PartialEq for TypedPtr<T, PTR> {
+    fn eq(&self, other: &Self) -> bool { self.ptr == other.ptr }
+}
+
+impl<T, PTR: PartialOrd> PartialOrd for TypedPtr<T, PTR> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> { self.ptr.partial_cmp(&other.ptr) }
 }
